d20381e81b5cbce7be08a2b396398f2160a15ff4
Damitha Perera <damithap@99x.io>
2024-02-28 20:03:00 +0530

    vercel

---
+++ ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apps : [{
+      name: "next-app",
+      script: "npm",
+      args: "start",
+      cwd: "node_modules/next/dist/bin/next",
+      watch: true
+    }]
+  };
