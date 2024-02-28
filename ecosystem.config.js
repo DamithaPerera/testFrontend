693f0a36bde797ce2bdaa4e93898c0eefad85648
Damitha Perera <damithap@99x.io>
2024-02-28 20:34:09 +0530

    vercel

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,9 @@ module.exports = {
       instances: 'max',
       script: 'node_modules/next/dist/bin/next',
       args: "start",
+      env_local: {
+        APP_ENV: 'local' // APP_ENV=local
+      },
       watch: true
     }]
   };
