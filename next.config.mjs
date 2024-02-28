ef077519ba7531bc91228362fb4c2a601a6b3fb9
Damitha Perera <damithap@99x.io>
2024-02-28 18:12:15 +0530

    cros

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    async rewrites() {
-        return [
-            {
-                source: '/v1/:path*', // Removed protocol and adjusted wildcard
-                destination: 'http://13.60.12.139:3000/v1/:path*', // Adjusted destination with the correct base URL
-            },
-        ]
-    },
-};
+const nextConfig = {};
 
 export default nextConfig;
